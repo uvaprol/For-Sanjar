ca626d7199525fe72c6be529628ea76f23e66cda
uvaprol <120366489+uvaprol@users.noreply.github.com>
2022-12-14 13:35:56 +0300

    Create MatrixTask.java

---
+++ MatrixTask.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        twoArray(x,y); //Вызов метода в коттором у нас реализован двумерный массив и его вывод
+    }
+    public static void twoArray(int x, int y){
+        int [][] twoArr= new int [y][x];
+        for(int i = 0; i < y; i++) {
+            for(int j=0; j < x; j++){
+                Scanner scanner = new Scanner(System.in);
+                int s = scanner.nextInt();
+                twoArr[i][j] = s;
+            }
+        }
+        System.out.println("Двухмерный массив twoArr:  " + Arrays.deepToString(twoArr)); //Вывод двумерного массива через вспомогательный метод Arrays
+    }
+}
